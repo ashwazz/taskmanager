7f14776731cd88371b436f56a796c0f0d037ce06
ashwazz <asvin.chan08@outlook.com>
2023-03-30 11:18:27 +0530

    git push

--- lib/UI/HomePage.dart
+++ lib/UI/HomePage.dart
@@ -1,12 +1,16 @@
 // ignore_for_file: prefer_const_constructors, library_private_types_in_public_api, prefer_typing_uninitialized_variables, duplicate_ignore
+import 'package:date_picker_timeline/date_picker_timeline.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:invert_colors/invert_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:taskmanager/UI/add_task_bar.dart';
 import 'package:taskmanager/UI/widgets/button.dart';
 import 'package:taskmanager/services/theme_services.dart';
 import 'package:get/get.dart';
 import '../services/notification_services.dart';
 import 'theme.dart';
+
 
 
 class HomePage extends StatefulWidget {
@@ -18,6 +22,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   var notifyHelper;
+  DateTime _selectedDate= DateTime.now() ;
 
   get isDarkMode => null;
 
@@ -33,7 +38,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Get.isDarkMode ?Colors.grey.shade800:Colors.white,
+      backgroundColor: Get.isDarkMode ? Colors.grey.shade800 : Colors.white,
       appBar: _appbar(),
       body: Column(
         // ignore: prefer_const_literals_to_create_immutables
@@ -41,7 +46,7 @@ class _HomePageState extends State<HomePage> {
           Row(
             children: [
               Container(
-                margin:
+                margin: // shows date
                     const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -50,28 +55,88 @@ class _HomePageState extends State<HomePage> {
                       DateFormat.yMMMMd().format(DateTime.now()),
                       style: subHeadingStyle,
                     ), // for date
-                    Text("Today", 
-                    style: TextStyle(
-                      fontSize: 20,
-                      color:Get.isDarkMode?Colors.grey.shade50: Colors.grey.shade800,
-                      fontStyle:  FontStyle.italic,
-                      fontWeight: FontWeight.bold,
-                      
-                    ),
-
+                    Text(
+                      "Today",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Get.isDarkMode
+                            ? Colors.grey.shade50
+                            : Colors.grey.shade800,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ],
                 ),
               ),
-              MyButton(label:"press" , onTap: ()=>null),
-            ],
 
-          )
+              // button /button / button
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: MyButton(
+                 label: "+add task ",
+                 
+                 onTap: ()=>Get.to(AddTaskPage()),
+              )
+              ),
+            ],
+          ),
+          Column(
+            // date picker
+            children: [
+              _addDateBar(),
+              Container(
+                margin: const EdgeInsets.only(top: 20),
+                child: DatePicker(
+                  DateTime.now(),
+                  height: 100,
+                  width: 75,
+                  initialSelectedDate: DateTime.now(),
+                  selectionColor: Get.isDarkMode
+                      ? Colors.blue.shade300
+                      : Colors.blue.shade500,
+                  selectedTextColor: Colors.black,
+                  dateTextStyle: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: Get.isDarkMode
+                        ? Colors.grey.shade50
+                        : Colors.grey.shade800,
+                  ),
+                  dayTextStyle: GoogleFonts.lato(
+                      textStyle: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                    color: Get.isDarkMode
+                        ? Colors.grey.shade50
+                        : Colors.grey.shade800,
+                  )),
+                  monthTextStyle: GoogleFonts.lato(
+                      textStyle: TextStyle(
+                    fontSize: 11,
+                    fontWeight: FontWeight.w600,
+                    color: Get.isDarkMode
+                        ? Colors.grey.shade50
+                        : Colors.grey.shade800,
+                  )
+                  ),
+                  onDateChange:(date){
+                    _selectedDate = date;
+                  
+                  },
+
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
   }
-
+  _addDateBar(){
+    return Container();
+  }
+  
   _appbar() {
     return AppBar(
       elevation: 3,
@@ -95,7 +160,7 @@ class _HomePageState extends State<HomePage> {
           padding: const EdgeInsets.fromLTRB(15.0, 0, 0, 0),
           child: Icon(
             Get.isDarkMode ? Icons.light_mode : Icons.nights_stay_sharp,
-            size: 45,
+            size: 30,
           ),
         ),
       ),
